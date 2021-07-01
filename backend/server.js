dec4904196e9bb32117b70dd15f42e678335b40a
Aisha Gench <aisha.gench@gmail.com>
2021-07-01 01:37:11 -0500

    create user routes js

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,9 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to recipe application." });
 });
 
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
