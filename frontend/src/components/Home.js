0be26a364d1bcd44e62f76d1fb9b1800981a46ad
Aisha Gench <aisha.gench@gmail.com>
2021-07-07 21:02:48 -0500

    import RecipeService

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
-
-import UserService from "../services/user.service";
+import RecipeService from "../services/recipe.service";
 
 const Home = () => {
-  const [content, setContent] = useState("");
+  const [content, setContent] = useState([]);
 
   useEffect(() => {
-    UserService.getPublicContent().then(
+    RecipeService.getRecipe().then(
       (response) => {
         setContent(response.data);
+        console.log(response.data)
       },
       (error) => {
         const _content =
@@ -22,10 +22,22 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="container">
-      <header className="jumbotron">
-        <h3>{content}</h3>
-      </header>
+    <div className="result">
+      <ul className="results__list">
+        {content.map((data)=>{
+               return <li>
+                    <a className="results__link results__link--active" href="#23456">
+                        <figure className="results__fig">
+                            <img src={data.image_url} alt={data.title}/>
+                        </figure>
+                        <div className="results__data">
+                            <h4 className="results__name">{data.title}</h4>
+                            <p className="results__author">{data.publisher}</p>
+                        </div>
+                    </a>
+                </li>
+        })}
+      </ul>
     </div>
   );
 };
